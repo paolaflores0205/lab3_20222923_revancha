e91d98b77f2b936bdae84ddc934ec0232e34cf71
unknown <a20222923@pucp.edu.pe>
2026-03-09 19:52:16 -0500

    empezando lab

---
+++ src/test/java/com/example/lab3241revancha/Lab3241RevanchaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab3241revancha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab3241RevanchaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
