6ac45c2d67b2e8a0bb516f4a3ea894fd98b33441
unknown <a20222923@pucp.edu.pe>
2026-03-11 13:21:33 -0500

    Parte de los oftalmologos recien terminada

--- src/main/java/com/example/lab3241revancha/controller/OftalmologoController.java
+++ src/main/java/com/example/lab3241revancha/controller/OftalmologoController.java
@@ -2,6 +2,7 @@ package com.example.lab3241revancha.controller;
 
 import com.example.lab3241revancha.entity.Clinica;
 import com.example.lab3241revancha.entity.Oftalmologo;
+import com.example.lab3241revancha.entity.Paciente;
 import com.example.lab3241revancha.repository.ClinicaRepository;
 import com.example.lab3241revancha.repository.OftalmologoRepository;
 import com.example.lab3241revancha.repository.PacienteRepository;
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -34,6 +36,22 @@ public class OftalmologoController {
         model.addAttribute("listaOftalmologos",listaOftalmologos);
         return "oftalmologos/lista";
     }
+
+    @GetMapping("/mostrarPacientes")
+    public String listarPacientes(Model model, @RequestParam("idOft") int idOft){
+        List<Paciente> listPxByOft = pacienteRepository.findPacienteByOftalmologoId(idOft);
+        model.addAttribute("listaPacientesByIdOft",listPxByOft);
+        return "oftalmologos/pacientesByOft";
+    }
+
+    @GetMapping("/proximasCitas")
+    public String listarProximasCitas(Model model, @RequestParam("idOft") int idOft){
+        List<Paciente> listProximasCitas = pacienteRepository.findPxByProximasCitas(idOft);
+        model.addAttribute("listaProximasCitas",listProximasCitas);
+        return "oftalmologos/pacientesByProximasCitas";
+    }
+
+
 
 
 }
--- src/main/java/com/example/lab3241revancha/repository/PacienteRepository.java
+++ src/main/java/com/example/lab3241revancha/repository/PacienteRepository.java
@@ -3,10 +3,19 @@ package com.example.lab3241revancha.repository;
 
 import com.example.lab3241revancha.entity.Paciente;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface PacienteRepository extends JpaRepository<Paciente, Integer> {
 
     List<Paciente> findByClinicaId(Integer clinicaId);
+
+    List<Paciente> findPacienteByOftalmologoId(Integer oftalmologoId);
+
+    @Query(value="SELECT * FROM paciente p WHERE p.oftalmologo_id = ?1 " +
+            "AND p.fecha_cita >= CURRENT_DATE " +
+            "ORDER BY p.fecha_cita ASC",
+            nativeQuery = true)
+    List<Paciente> findPxByProximasCitas (Integer oftId);
 }
